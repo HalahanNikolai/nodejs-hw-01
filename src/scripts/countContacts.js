dc69fb5f06c7bfd159687cf05269a12cef264573
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2025-02-23 23:56:46 +0200

    Step_6

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,14 @@
-export const countContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'fs/promises';
+
+export const countContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, { encoding: 'utf-8', });
+        const contacts = JSON.parse(data);
+        return contacts.reduce((total, contact) => { return total + 1; }, 0);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 console.log(await countContacts());
